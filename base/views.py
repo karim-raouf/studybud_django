661b29a656ac25fe5cf74848da581f19396e4167
karim-raouf <karimraoufm@gmail.com>
2023-07-08 17:53:42 +0200

    just started made views for home and rooms

---
+++ base/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+rooms = [
+    {'id' : 1 , 'name' : 'lets learn python'},
+    {'id' : 2 , 'name' : 'design with me'},
+    {'id' : 3 , 'name' : 'frontend developers'},
+]
+
+
+
+def home(request):
+    context = {'rooms' : rooms}
+    return render(request , 'base/home.html' , context)
+
+
+
+def room(request , id):
+    room = None
+    for i in rooms:
+        if i['id'] == id:
+            room = i
+    context = {'room' : room}        
+
+    return render(request , 'base/room.html' , context)
+
